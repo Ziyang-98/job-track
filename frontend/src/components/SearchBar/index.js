6f9a823769a0c7fec05ca0d738402fe690d09d9b
Lim Zi Yang <yang7332@gmail.com>
2024-09-05 19:50:06 +0800

    feat: add clear search filter icon

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,23 +1,36 @@
-import { TextField } from "@mui/material";
+import { IconButton, TextField } from "@mui/material";
 import InputAdornment from "@mui/material/InputAdornment";
+import ClearIcon from "@mui/icons-material/Clear";
+const SearchBar = ({ searchFilter, setSearchFilter }) => {
+  const clearSearchFilter = () => {
+    setSearchFilter("");
+  };
 
-const SearchBar = ({ searchFilter, setSearchFilter }) => (
-  <TextField
-    className="text"
-    onInput={(e) => {
-      setSearchFilter(e.target.value);
-    }}
-    variant="outlined"
-    placeholder="Search Role or Company"
-    size="small"
-    sx={{ width: "20rem" }}
-    value={searchFilter}
-    slotprops={{
-      input: {
-        endAdornment: <InputAdornment position="end">kg</InputAdornment>,
-      },
-    }}
-  />
-);
+  return (
+    <TextField
+      className="text"
+      onInput={(e) => {
+        setSearchFilter(e.target.value);
+      }}
+      variant="outlined"
+      placeholder="Search Role or Company"
+      size="small"
+      sx={{ width: "20rem" }}
+      value={searchFilter}
+      slotProps={{
+        input: {
+          endAdornment: searchFilter && (
+            <InputAdornment position="end">
+              <IconButton onClick={clearSearchFilter}>
+                <ClearIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+          sx: { pr: 0.5 },
+        },
+      }}
+    />
+  );
+};
 
 export default SearchBar;
