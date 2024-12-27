37f34db50f1211eddc79de254f1a4178db55ecee
Lim Zi Yang <yang7332@gmail.com>
2024-12-27 17:01:16 +0800

    feat: add loading text

--- frontend/src/components/LoadingScreen/index.js
+++ frontend/src/components/LoadingScreen/index.js
@@ -1,10 +1,13 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { styles } from "./styles";
 
 export default function LoadingScreen() {
   return (
     <Box sx={styles.loadingOverlay}>
       <CircularProgress sx={styles.loadingIcon} />
+      <Typography sx={styles.loadingText} variant="h6">
+        Loading job applications...
+      </Typography>
     </Box>
   );
 }
--- frontend/src/components/LoadingScreen/styles.js
+++ frontend/src/components/LoadingScreen/styles.js
@@ -3,14 +3,20 @@ export const styles = {
     position: "absolute",
     inset: 0,
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    gap: 2,
     backgroundColor: "primary.light",
-    opacity: 0.7,
+    opacity: 0.8,
     zIndex: 1,
     borderRadius: 4,
   },
   loadingIcon: {
     color: "secondary.dark",
+  },
+  loadingText: {
+    color: "secondary.contrastText",
+    fontWeight: "bolder",
   },
 };
